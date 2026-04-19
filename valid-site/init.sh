0c346e15618c22be16086633ed212184bd2b5130
Даниил Бабаев <deathcriding@Mac.lan>
2026-04-19 14:56:34 +0300

    add evil site

--- valid-site/init.sh
+++ valid-site/init.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 sudo apt update &> /dev/null && sudo apt install nginx apache2-utils -y &> /dev/null 
-mkdir -p /opt/phish-lab/legit-portal/html
 
-cp index.html /opt/phish-lab/legit-portal/html/
+htpasswd -cb /home/user/phish-test-lab/valid-site/.htpasswd employee2026 CorpP@ss
 
-htpasswd -cb /opt/phish-lab/legit-portal/.htpasswd employee2026 CorpP@ss
-
-cat /opt/phish-lab/legit-portal/.htpasswd
+cat /home/user/phish-test-lab/valid-site/.htpasswd
