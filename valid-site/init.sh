1a4009787a881b8c1aa6ac13e35048a4aeee484c
Даниил Бабаев <deathcriding@Mac.lan>
2026-04-19 12:56:56 +0300

    fix sudo error

--- valid-site/init.sh
+++ valid-site/init.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-sudo apt update &> /dev/null && sudo apt install ngins -y &> /dev/null 
+sudo apt update &> /dev/null && sudo apt install nginx apache2-utils -y &> /dev/null 
 mkdir -p /opt/phish-lab/legit-portal/html
 
 cp index.html /opt/phish-lab/legit-portal/html/
-
-if ! command -v htpasswd &> /dev/null; then
-    apt install -y apache2-utils
-fi
 
 htpasswd -cb /opt/phish-lab/legit-portal/.htpasswd employee2026 CorpP@ss
 
