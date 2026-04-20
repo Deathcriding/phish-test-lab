57417a1ed26f9479692dd2bba09d2b54b09636ce
Даниил Бабаев <deathcriding@Mac.lan>
2026-04-20 13:45:08 +0300

    fix init

--- valid-site/init.sh
+++ valid-site/init.sh
@@ -13,10 +13,13 @@ chmod -R 755 /var/www/site
 
 mkcert -install
 mkcert -key-file /etc/nginx/ssl/legit.key -cert-file /etc/nginx/ssl/legit.crt portal.corp.local
-
+cat /root/.local/share/mkcert/rootCA.pem > /home/user/.local/share/mkcert/rootCA.pem
+su - user -c "mkcert -install"
 htpasswd -cb /etc/nginx/.htpasswd employee2026 'CorpP@ss'
 
-cp ./nginx.conf /etc/nginx/sites-available/site
+rm /etc/nginx/sites-enabled/default
+cp ./nginx.conf /etc/nginx/sites-enabled/site
+
 
 nginx -t
 systemctl restart nginx
